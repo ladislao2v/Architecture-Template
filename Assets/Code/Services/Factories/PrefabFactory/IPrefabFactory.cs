dd42b67a1e874563c5ee6daf0b35756338a97fe0
v.khobian <v.khobian@emehanica.com>
2024-07-13 15:44:46 +0300

    Template uploaded

---
+++ Assets/Code/Services/Factories/PrefabFactory/IPrefabFactory.cs
@@ -0,0 +1,11 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Code.Services.Factories.PrefabFactory
+{
+    public interface IPrefabFactory
+    {
+        UniTask<T> Create<T>() where T : MonoBehaviour;
+        T Create<T>(T prefab) where T : MonoBehaviour;
+    }
+}
---
+++ Assets/Code/StateMachine/States/BootState.cs
@@ -0,0 +1,29 @@
+﻿using Code.Services.Factories.StateFactory;
+using Code.Services.SceneLoader;
+using Code.StateMachine.Core.Interfaces;
+using UnityEngine;
+
+namespace Code.StateMachine.States
+{
+    public class BootState : IState
+    {
+        private readonly IStateMachine _stateMachine;
+        private readonly ISceneLoader _sceneLoader;
+
+        public BootState(IStateMachine stateMachine, ISceneLoader sceneLoader)
+        {
+            _stateMachine = stateMachine;
+            _sceneLoader = sceneLoader;
+        }
+        
+        public async void Enter()
+        {
+            Debug.Log("BootState enter");
+        }
+
+        public async void Exit()
+        {
+            Debug.Log("BootState exit");
+        }
+    }
+}
